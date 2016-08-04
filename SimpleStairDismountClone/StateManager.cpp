200fb3b88ec46a140134a5011da0ff99becd019f
Kelvin Lau <kclau95@gmail.com>
2016-08-04 13:12:58 +1200

    Implemented constructor and destructor for StateManager
    Also implemented getCurrentState()

--- SimpleStairDismountClone/StateManager.cpp
+++ SimpleStairDismountClone/StateManager.cpp
@@ -1,19 +1,27 @@
 #include "StateManager.h"
-#include "State.h"
+#include "State.h"		  
+#include "StateReady.h"
+#include "StateDismount.h"
 
 StateManager::StateManager()
 {
+	_states[0] = new StateReady();
+	_states[1] = new StateDismount();
+	currentState = _states[0];
 }
 
 
 StateManager::~StateManager()
 {
+	for (int i = 0; i < 2;i++) {
+		delete _states[i];
+	}
+	delete _states;
 }
 
 State *StateManager::getCurrentState()
 {
-	//return &currentState;
-	return nullptr;
+	return currentState;
 }
 
 void StateManager::goTo(state nextState)
