f686eff294ce81d220da42f0696620a7cefde574
Kelvin Lau <kclau95@gmail.com>
2016-08-07 01:35:15 +1200

    Commenting on what body parts and constraints i need

--- SimpleStairDismountClone/Ragdoll.cpp
+++ SimpleStairDismountClone/Ragdoll.cpp
@@ -1,6 +1,7 @@
 #include "Ragdoll.h"
 
-
+// head, neck, torso, abdomen, pelvis; left and right upper arm, forearm, thigh, leg, foot
+// joint head-neck, neck-torso, torso-abdomen, abdomen-pelvis, torso and left right upper arm, left right upper arm and forearm, pelvis and left and right thigh, left and right thigh and leg, left and right leg and foot
 
 Ragdoll::Ragdoll()
 {
