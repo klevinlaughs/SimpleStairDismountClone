1fb5ffce8a00fc72e9a563e80735783d39da9e76
Kelvin Lau <kclau95@gmail.com>
2016-08-07 17:00:21 +1200

    Using enums like demo for body parts

--- SimpleStairDismountClone/Ragdoll.h
+++ SimpleStairDismountClone/Ragdoll.h
@@ -3,7 +3,27 @@
 #include "BulletObject.h"
 class Ragdoll
 {
-private:
+private:	// head, neck, torso, abdomen, pelvis; left and right upper arm, forearm, thigh, leg, foot
+	enum bodyParts 
+	{
+		HEAD = 0, 
+		NECK, 
+		TORSO,
+		ABDOMEN,
+		PELVIS,
+		ARM_UPPER_LEFT,
+		ARM_UPPER_RIGHT,
+		ARM_LOWER_LEFT,
+		ARM_LOWER_RIGHT,
+		THIGH_LEFT,
+		THIGH_RIGHT,
+		LEG_LEFT,
+		LEG_RIGHT,
+		FOOT_LEFT,
+		FOOT_RIGHT,
+		
+		BODYPART_COUNT
+	};
 	btDiscreteDynamicsWorld *world;
 	BulletObject *bodyParts[15];
 public:
